47d9a852c86f65263bd4a680c80bc3127d2d12b3
Scott Adams <sradms0@protonmail.com>
2025-12-22 13:34:45 -0500

    1: Add documentation.

--- src/Infrastructure/Dataframework.Infrastructure.Primitives/EntityBase.cs
+++ src/Infrastructure/Dataframework.Infrastructure.Primitives/EntityBase.cs
@@ -2,7 +2,17 @@
 
 namespace Dataframework.Infrastructure.Primitives;
 
+/// <summary>
+/// Provides a minimal base implementation for entities that require a
+/// strongly-typed identifier.
+/// </summary>
+/// <typeparam name="TId">
+/// The identifier type for the entity.
+/// </typeparam>
 public abstract class EntityBase<TId> : IId<TId>
 {
+    /// <summary>
+    /// Gets or sets the unique identifier for the entity.
+    /// </summary>
     public TId Id { get; set; } = default!;
 }
